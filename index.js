655c81a24108137fc6abb976e857297d55749b44
rpatel07 <157613448+rpatel07@users.noreply.github.com>
2024-08-20 16:52:10 -0400

    Implement Global Weather Tracker application with search and display functionality

--- index.js
+++ index.js
@@ -4,31 +4,36 @@ import axios from "axios";
 
 const port = 3000;
 const app = express();
-const URL = "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather";
-const API = "23f47a09d4msh95edea3a961ca40p119fc8jsnf948409fe292";
+const API_KEY = "23f47a09d4msh95edea3a961ca40p119fc8jsnf948409fe292";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.listen(port,()=>{console.log(`listening on ${port}`)});
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
-  });
+});
 
 app.post("/weather", async (req, res) => {
+    const cityName = req.body.city;
+    const options = {
+        method: 'GET',
+        url: `https://open-weather13.p.rapidapi.com/city/${cityName}/EN`,
+        headers: {
+            'x-rapidapi-key': API_KEY,
+            'x-rapidapi-host': 'open-weather13.p.rapidapi.com'
+        }
+    };
+
     try {
-        const cityName = req.body.city;
-        console.log(cityName);
-        const result = await axios.get(URL, {
-            params: { city: cityName },
-            headers: {
-                'X-RapidAPI-Key': API,
-                'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-            }
-        });
-        console.log(result.data);
-        res.render("index.ejs", { info: result.data,placeName:cityName });
+        const response = await axios.request(options);
+        res.render("index.ejs", { info: response.data, placeName: cityName });
+        console.log(response.data);
     } catch (error) {
+        console.error(error.response ? error.response.data : error.message);
         res.status(404).send(error.message);
     }
 });
